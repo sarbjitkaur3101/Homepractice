d164768f92a74b499d68217eb7e707f1f4ca2bc0
sarbjit kaur <nishuuukaur12@gmail.com>
2020-09-22 14:43:17 -0400

    Bottom Navigation item selected listener done

--- app/src/main/java/com/sarb/homepractice/HomeActivity.java
+++ app/src/main/java/com/sarb/homepractice/HomeActivity.java
@@ -1,14 +1,62 @@
 package com.sarb.homepractice;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeActivity extends AppCompatActivity {
 
+    BottomNavigationView bottomNavigationView;
+    NavController navController;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        bottomNavigationView= findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(menu);
     }
+
+    BottomNavigationView.OnNavigationItemSelectedListener menu=new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            switch (item.getItemId()){
+
+                case R.id.home_menu:
+                    navController=Navigation.findNavController(HomeActivity.this,R.id.home_host_nav);
+                    navController.navigate(R.id.homeFragment);
+                     break;
+                case R.id.cart_menu:
+                    navController=Navigation.findNavController(HomeActivity.this,R.id.home_host_nav);
+                    navController.navigate(R.id.cartFragment);
+                                 break;
+
+                case R.id.profile_menu:
+                    navController=Navigation.findNavController(HomeActivity.this,R.id.home_host_nav);
+                    navController.navigate(R.id.profileFragment);
+
+                                 break;
+
+
+
+
+            }
+
+
+
+            return true;
+        }
+    };
+
+
+
 }
